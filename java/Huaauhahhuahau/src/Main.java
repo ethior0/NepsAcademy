ce66d70db1d43bd09f20a9582b986cc0e538c345
ethior0 <thiagomeigmoreira18@gmail.com>
2024-01-30 22:40:17 -0300

    hahaha

---
+++ java/Huaauhahhuahau/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner ler = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		
+		String risadaR, risada = ler.next();
+		risada = risada.replaceAll("[^aeiou]", "");
+		sb.append(risada);
+		risadaR = sb.reverse().toString();
+		
+		System.out.println(risada.equals(risadaR) ? "S" : "N");
+	}
+}
